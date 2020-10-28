e11610abe1a51efff86b3f85367d6ccfb2d7e237
qdesnik <pivovarov.vasiliy.np@gmail.com>
2020-10-28 22:26:43 +0300

    start

---
+++ Assets/Scripts/CheckRegion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+
+public class CheckRegion : MonoBehaviour
+{
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            if (hit)
+            {
+                if (hit.collider.TryGetComponent(out Region region))
+                {
+                    FocusOn(region);
+                    region.OnCheck();
+                }
+            }
+        }
+    }
+
+    private void FocusOn(Region region)
+    {
+        _camera.transform.position = region.transform.position + new Vector3(0, 0, _camera.transform.position.z);
+        _camera.orthographicSize = 3;
+    }
+}
---
+++ Assets/Scripts/Region.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Region : MonoBehaviour
+{
+    [SerializeField] private int _population;
+
+    private Color _defaultColor;
+    private Renderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    public void OnCheck()
+    {
+        _renderer.material.color = Color.red;
+    }
+}
