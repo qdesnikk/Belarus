9c5de5a90b80fa2d0b9720802f9e13fed97d51ce
qdesnik <pivovarov.vasiliy.np@gmail.com>
2020-11-12 00:13:48 +0300

    add tutorial, fix canvas size

---
+++ Assets/Scripts/UI/ShowTutorial.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTutorial : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _windows;
+    private int _currentWindow; 
+
+    private void OnEnable()
+    {
+        _currentWindow = 0;
+        _windows[_currentWindow].SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            NextWindow();
+        }
+    }
+
+    private void NextWindow()
+    {
+        _windows[_currentWindow].SetActive(false);
+        _currentWindow++;
+
+        if (_currentWindow < _windows.Count)
+        {
+            _windows[_currentWindow].SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            this.gameObject.SetActive(false);
+            return;
+        }
+    }
+}
